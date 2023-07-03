d007644d022b2fe418261be66e0a126c3b1c14bb
yuanlin2008 <yuanlin2008@hotmail.com>
2023-07-03 22:20:55 +0800

    nextauth

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,17 @@
+import prisma from "@/lib/prisma";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { AuthOptions } from "next-auth";
+import NextAuth from "next-auth/next";
+import GithubProvider from "next-auth/providers/github";
+
+export const Options:AuthOptions = {
+	providers:[
+		GithubProvider({
+			clientId:process.env.GITHUB_CLIENT_ID as string,
+			clientSecret:process.env.GITHUB_CLIENT_SECRET as string,
+		}),
+	],
+	adapter: PrismaAdapter
+}
+
+export default NextAuth(Options);
