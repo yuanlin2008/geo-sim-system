ce79baa36b3d0245ec06c4c9d669a514bd126735
yuanlin2008 <yuanlin2008@hotmail.com>
2023-07-12 15:06:29 +0800

    123

--- src/app/(auth)/layout.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react"
-
-const layout = ({ children }: { children: React.ReactNode }) => {
-  return <div className="flex min-h-screen flex-col ">{children}</div>
-}
-
-export default layout
