b4bbd5a4772f9791ad34f90027a2c9eb2b98ec48
yuanlin2008 <yuanlin2008@hotmail.com>
2023-07-26 23:52:48 +0800

    123

--- src/app/(protected)/admin/page.tsx
+++ src/app/(protected)/admin/page.tsx
@@ -1,9 +1,21 @@
+"use client"
+
 import React from "react"
+import ReactFlow from "reactflow"
 
-type Props = {}
+import "reactflow/dist/style.css"
 
-const Page = (props: Props) => {
-  return <div>Home</div>
+const initialNodes = [
+  { id: "1", position: { x: 0, y: 0 }, data: { label: "1" } },
+  { id: "2", position: { x: 0, y: 100 }, data: { label: "2" } },
+  { id: "3", position: { x: 100, y: 100 }, data: { label: "3" } },
+]
+const initialEdges = [{ id: "e1-2", source: "1", target: "2" }]
+
+export default function Page() {
+  return (
+    <div style={{ width: "100%", height: "100vh" }}>
+      <ReactFlow nodes={initialNodes} edges={initialEdges} />
+    </div>
+  )
 }
-
-export default Page
